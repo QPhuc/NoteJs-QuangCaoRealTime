ca32f1bd49c49a12b01b4b303ebc468861fe8b6b
Qphuc <tqp1610@gmail.com>
2021-08-14 00:08:00 +0700

    admin.ejs

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var express = require("express");
 var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.set("views", "./view");
+app.set("views", "./views");
 
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
 
+var mangAds = [
+    new QuangCao("gongcha.jpg", "http://drthanh.com"),
+    new QuangCao("drthanh.jpg", "http://gongcha.com"),
+    new QuangCao("vinamilk.jpg", "http://vinamilk.com"),
+]
+
 app.get("/admin", function (req, res) {
-    res.send("Hello Admin!");
+    res.render("admin", { mangQC: mangAds });
 });
 
+function QuangCao(hinh, link) {
+    this.Hinh = hinh;
+    this.Link = link;
+}
